a1d02a9259f3244ff2034e922e96947b02c96c43
esoastor <hazehelt@gmail.com>
2023-03-18 10:39:13 +0300

    update_tag updated

--- update_tag.sh
+++ update_tag.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 OLD_TAG=$(git describe --tags --abbrev=0)
-NEW_LAST_NUMBER=$((${OLD_TAG:0-1} + 1))
+LAST_NUMBER=${OLD_TAG: -1}
+if [ $LAST_NUMBER -eq 9 ]; then
+  NEW_LAST_NUMBER="00"
+else
+  NEW_LAST_NUMBER=$(($LAST_NUMBER + 1))
+fi
 NEW_TAG="${OLD_TAG::-1}${NEW_LAST_NUMBER}"
 
 git tag $NEW_TAG
